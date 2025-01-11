c17b31abbebde3edd0a0ab7b29141fd61c4679da
B95segal <193628847+B95segal@users.noreply.github.com>
2025-01-11 13:29:40 +1100

    debug

--- run.ps1
+++ run.ps1
@@ -1,9 +1,10 @@
+# Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-      if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-            $CommandLine = "-NoExit -c cd '$pwd'; & `"" + $MyInvocation.MyCommand.Path + "`""
-            Start-Process powershell -Verb runas -ArgumentList $CommandLine
-            Exit
-      }
+	if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+				$CommandLine = "-NoExit -c cd '$pwd'; & `"" + $MyInvocation.MyCommand.Path + "`""
+				Start-Process powershell -Verb runas -ArgumentList $CommandLine
+				Exit
+	}
 }
 
 #Elevated script content after that
