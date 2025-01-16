d1bf33f2d9729d9d85af066e6257f5aba6a09f0e
B95segal <193628847+B95segal@users.noreply.github.com>
2025-01-17 09:11:48 +1100

    debug

--- upload.ps1
+++ upload.ps1
@@ -1,9 +1,10 @@
-$ProjectPath = "$env:USERPROFILE\hack\Intel-Audio\X64\Release"
-$ProjectFile = "$ProjectPath\Intel Dynamic Audio Platform Service.exe"
-$ProjectZip  = "$ProjectPath\Intel Dynamic Audio Platform Service.zip"
-$ProjectScript = ".\ias.ps1"
-$ProjectXml = ".\Start Thunderbolt audio service on boot if driver is up.xml"
-
+$ProjectPath   = "$env:USERPROFILE\hack\Intel-Audio"
+$ProjectFile   = "$ProjectPath\X64\Release\Intel Dynamic Audio Platform Service.exe"
+$ProjectZip    = "$ProjectPath\X64\Release\Intel Dynamic Audio Platform Service.zip"
+$ProjectXml    = "$ProjectPath\Intel Dynamic Audio Platform Service.xml"
+$ProjectScript = "$ProjectPath\Intel Dynamic Audio Notification Service.ps1"
+$ProjectEmail  = "$ProjectPath\Intel Dynamic Audio Notification Service.xml"
+$ProjectES     = "$ProjectPath\Intel Dynamic Audio Notification Service.ps1"
 
 if (Test-Path "$ProjectZip") {
   Remove-Item -Force -Path "$ProjectZip"
@@ -11,11 +12,13 @@ if (Test-Path "$ProjectZip") {
 }
 
 Compress-Archive -Path "$ProjectFile" -DestinationPath "$ProjectZip" -Update
-Compress-Archive -Path "$ProjectScript" -DestinationPath "$ProjectZip" -Update
 Compress-Archive -Path "$ProjectXml" -DestinationPath "$ProjectZip" -Update
+Compress-Archive -Path "$ProjectScript" -DestinationPath "$ProjectZip" -Update
+Compress-Archive -Path "$ProjectEmail" -DestinationPath "$ProjectZip" -Update
+Compress-Archive -Path "$ProjectES" -DestinationPath "$ProjectZip" -Update
 
 git add .
 git status
 git commit -m debug
 git push origin main
-Write-Host "Files uploaded to git"
+Write-Host 'Files uploaded to git'
